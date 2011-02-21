5b2a339f481da21773f68ed39f013fd6c5b76d7b
Vincent Barrier <vincent.barrier@icescrum.com>
2011-02-21 12:33:47 +0100

    update

--- FluxiableGrailsPlugin.groovy
+++ FluxiableGrailsPlugin.groovy
@@ -7,11 +7,9 @@ import grails.plugin.fluxiable.Activity
 class FluxiableGrailsPlugin {
   def groupId = "org.icescrum"
 
-  def version = "0.1"
+  def version = "0.3"
   // the version or versions of Grails the plugin is designed for
-  def grailsVersion = "1.3 > *"
-  // the other plugins this plugin depends on
-  def dependsOn = [hibernate: "1.3 > *"]
+  def grailsVersion = "1.3.7 > *"
   // resources that are excluded from plugin packaging
   def pluginExcludes = [
           "grails-app/views/error.gsp"
